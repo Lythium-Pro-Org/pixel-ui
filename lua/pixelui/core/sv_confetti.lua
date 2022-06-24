20c928c595d652a2f0836a617badbcfea5c8e9b5
Lythium <62567544+Lythium4848@users.noreply.github.com>
2022-06-24 21:25:05 +0100

    refactor: remove useless code

--- lua/pixelui/core/sv_confetti.lua
+++ lua/pixelui/core/sv_confetti.lua
@@ -7,8 +7,4 @@ do
         util.Effect("pixel_confetti", effectData)
         sound.Play("pixelui-sounds/pixel_confetti.mp3", ply:GetPos())
     end
-
-    net.Receive("PIXEL.SpawnConfetti", function(_, ply)
-        PIXEL.SpawnConfetti(ply)
-    end)
 end
