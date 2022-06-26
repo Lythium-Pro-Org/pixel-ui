04c90ed37b2ca1c0b6f9b4b815ff775658f9dd18
Lythium <62567544+Lythium4848@users.noreply.github.com>
2022-06-26 15:05:58 +0100

    feat: Allow servers to have custom themes without breaking shit

--- lua/pixelui/panels/cl_themeeditor.lua
+++ lua/pixelui/panels/cl_themeeditor.lua
@@ -1,8 +1,8 @@
 local PANEL = {}
 local sc = PIXEL.Scale
-
+PIXEL.GenerateFont(18)
 function PANEL:Init()
-    self:SetSize(sc(200), sc(175))
+    self:SetSize(sc(200), sc(200))
     self:MakePopup()
     self:Center()
 
@@ -11,7 +11,7 @@ function PANEL:Init()
     local panW, panH = self:GetSize()
 
     self.DropDown = self:Add("PIXEL.ComboBox")
-    self.DropDown:SetPos((panW / 2) - (self.DropDown:GetWide() / 2), panH / 2 - sc(15))
+    self.DropDown:SetPos((panW / 2) - (self.DropDown:GetWide() / 2), panH / 2 - sc(35))
     self.DropDown:SetSizeToText(false)
 
     for k, v in pairs(PIXEL.Themes) do
@@ -19,9 +19,29 @@ function PANEL:Init()
     end
     self.DropDown:SetValue(LocalPlayer().PIXELTheme)
 
-    local themeSelected
+
+    self.SaveCross = self:Add("PIXEL.LabelledCheckbox")
+    self.SaveCross:SetPos((panW / 2) - (self.SaveCross:GetWide() / 2) - sc(50), panH / 2 + sc(10))
+    self.SaveCross:SetText("Save Across Servers?")
+    self.SaveCross:SetFont("PIXEL.Font.Size18")
+    local crossServer = false
+    if not table.HasValue(PIXEL.DefaultThemes, LocalPlayer().PIXELTheme) then
+        self.SaveCross.Checkbox:SetEnabled(false)
+        crossServer = false
+    end
+
+
+    local themeSelected = LocalPlayer().PIXELTheme
+
     function self.DropDown:OnSelect(index, text, data)
         themeSelected = data
+        if PIXEL.Themes[themeSelected].included then
+            self:GetParent().SaveCross.Checkbox:SetEnabled(true)
+            crossServer = true
+        else
+            self:GetParent().SaveCross.Checkbox:SetEnabled(false)
+            crossServer = false
+        end
     end
 
     self.Submit = self:Add("PIXEL.TextButton")
@@ -31,7 +51,7 @@ function PANEL:Init()
     self.Submit:SetText("Submit")
 
     self.Submit.DoClick = function()
-        PIXEL.SetTheme(themeSelected)
+        PIXEL.SetTheme(themeSelected, crossServer)
         self:Close()
         notification.AddLegacy("PIXELUI: Theme changed to " .. themeSelected, 0, 3)
     end
