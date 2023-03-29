c6140de6fe87ff52dbb5e5113affa369d1665877
Lythium4848 <62567544+Lythium4848@users.noreply.github.com>
2023-03-29 21:53:31 +0100

    Update updater url

--- lua/autorun/sh_pixelui_loader.lua
+++ lua/autorun/sh_pixelui_loader.lua
@@ -92,11 +92,11 @@ resource.AddWorkshop("2825396224")
 hook.Add("Think", "PIXEL.UI.VersionChecker", function()
 	hook.Remove("Think", "PIXEL.UI.VersionChecker")
 
-	http.Fetch("https://raw.githubusercontent.com/Pulsar-Dev/pulsar-lib/master/VERSION", function(body)
+	http.Fetch("https://raw.githubusercontent.com/Pulsar-Dev/pixel-ui/master/VERSION", function(body)
 		if PIXEL.UI.Version ~= string.Trim(body) then
 			local red = Color(192, 27, 27)
 
-			MsgC(red, "[PIXEL UI] There is an update available, please download it at: https://github.com/Pulsar-Dev/pulsar-lib/releases/latest\n")
+			MsgC(red, "[PIXEL UI] There is an update available, please download it at: https://github.com/Pulsar-Dev/pixel-ui/releases/latest\n")
 			MsgC(red, "\nYour version: " .. PIXEL.UI.Version .. "\n")
 			MsgC(red, "New  version: " .. body .. "\n")
 			return
