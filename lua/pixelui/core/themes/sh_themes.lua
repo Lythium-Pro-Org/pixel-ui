cfbe06876af5511acf70ae689381218172d065f6
Lythium4848 <62567544+Lythium4848@users.noreply.github.com>
2023-04-25 17:18:06 +0100

    stuff

--- lua/pixelui/core/themes/sh_themes.lua
+++ lua/pixelui/core/themes/sh_themes.lua
@@ -1,22 +1,23 @@
 PIXEL.Colors = {
-    Background = Color(22, 22, 22),
-    Header = Color(28, 28, 28),
-    SecondaryHeader = Color(15, 15, 15),
+    Background = Color(31, 31, 31),
+    Header = Color(40, 41, 42),
+    SecondaryHeader = Color(45, 47, 49),
     Scroller = Color(61, 61, 61),
-
+    Outline = Color(55, 56, 57),
+    --
     PrimaryText = Color(255, 255, 255),
     SecondaryText = Color(220, 220, 220),
     DisabledText = Color(40, 40, 40),
-
-    Primary = Color(77, 79, 199),
+    --
+    Primary = Color(66, 133, 244),
+    --
     Disabled = Color(180, 180, 180),
     Positive = Color(68, 235, 124),
-    Negative = Color(235, 68, 68),
-
+    Negative = Color(179, 38, 30),
+    --
     Diamond = Color(185, 242, 255),
     Gold = Color(255, 215, 0),
     Silver = Color(192, 192, 192),
     Bronze = Color(145, 94, 49),
-
     Transparent = Color(0, 0, 0, 0)
 }
--- lua/pixelui/elements/cl_category.lua
+++ lua/pixelui/elements/cl_category.lua
@@ -16,9 +16,7 @@
 --]]
 
 local PANEL = {}
-
 AccessorFunc(PANEL, "Title", "Title", FORCE_STRING)
-
 PIXEL.RegisterFont("UI.CategoryHeader", "Open Sans Bold", 19)
 
 function PANEL:Init()
@@ -32,20 +30,17 @@ function PANEL:DoClick()
 end
 
 local lerp = Lerp
+
 function PANEL:Paint(w, h)
-    PIXEL.DrawRoundedBox(PIXEL.Scale(4), 0, 0, w, h, self.BackgroundCol)
+    PIXEL.DrawRoundedBox(PIXEL.Scale(8), 0, 0, w, h, self.BackgroundCol)
     PIXEL.DrawSimpleText(self.Title, "UI.CategoryHeader", PIXEL.Scale(10), h / 2, PIXEL.Colors.PrimaryText, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-
     self.ArrowRotation = lerp(FrameTime() * 10, self.ArrowRotation, self:GetParent():GetExpanded() and 0 or 90)
-
     local arrowSize = h * .45
     PIXEL.DrawImgurRotated(w - h * .3 - PIXEL.Scale(4), h / 2, arrowSize, arrowSize, self.ArrowRotation, "30Bvuwi", PIXEL.Colors.PrimaryText)
 end
 
 vgui.Register("PIXEL.CategoryHeader", PANEL, "PIXEL.Button")
-
 PANEL = {}
-
 AccessorFunc(PANEL, "m_bSizeExpanded", "Expanded", FORCE_BOOL)
 AccessorFunc(PANEL, "m_iContentHeight", "StartHeight")
 AccessorFunc(PANEL, "m_fAnimTime", "AnimTime")
@@ -55,15 +50,11 @@ AccessorFunc(PANEL, "m_pList", "List")
 
 function PANEL:Init()
     self.Header = vgui.Create("PIXEL.CategoryHeader", self)
-
     self:SetTitle("PIXEL Category")
-
     self:SetExpanded(true)
     self:SetMouseInputEnabled(true)
-
     self:SetAnimTime(0.2)
     self.SlideAnimation = Derma_Anim("Anim", self, self.AnimSlide)
-
     self.BackgroundCol = PIXEL.OffsetColor(PIXEL.Colors.Background, 2)
 
     self:InvalidateLayout(true)
@@ -71,6 +62,7 @@ end
 
 function PANEL:UnselectAll()
     local children = self:GetChildren()
+
     for k, v in pairs(children) do
         if v.SetSelected then
             v:SetSelected(false)
@@ -94,7 +86,6 @@ function PANEL:SetContents(contents)
     self.Contents = contents
     self.Contents:SetParent(self)
     self.Contents:Dock(FILL)
-
     local margin = PIXEL.Scale(8)
     self.Contents:DockMargin(margin, margin, margin, margin)
 
@@ -121,21 +112,24 @@ end
 function PANEL:Toggle()
     self:SetExpanded(not self:GetExpanded())
 
-    self.SlideAnimation:Start(self:GetAnimTime(), {From = self:GetTall()})
+    self.SlideAnimation:Start(self:GetAnimTime(), {
+        From = self:GetTall()
+    })
 
     self:InvalidateLayout(true)
-
     self:OnToggle(self:GetExpanded())
 end
 
-function PANEL:OnToggle(expanded) end
+function PANEL:OnToggle(expanded)
+end
 
 function PANEL:DoExpansion(b)
     if self:GetExpanded() == b then return end
     self:Toggle()
 end
 
-function PANEL:LayoutContent(w, h) end
+function PANEL:LayoutContent(w, h)
+end
 
 function PANEL:PerformLayout(w, h)
     self.Header:Dock(TOP)
@@ -151,20 +145,26 @@ function PANEL:PerformLayout(w, h)
     end
 
     if self:GetExpanded() then
-        if IsValid(self.Contents) and #self.Contents:GetChildren() > 0 then self.Contents:SizeToChildren(false, true) end
+        if IsValid(self.Contents) and #self.Contents:GetChildren() > 0 then
+            self.Contents:SizeToChildren(false, true)
+        end
+
         self:SizeToChildren(false, true)
     else
-        if IsValid(self.Contents) and not self.OldHeight then self.OldHeight = self.Contents:GetTall() end
+        if IsValid(self.Contents) and not self.OldHeight then
+            self.OldHeight = self.Contents:GetTall()
+        end
+
         self:SetTall(self.Header:GetTall())
     end
 
     self.SlideAnimation:Run()
-
     self:LayoutContent(w, h)
 end
 
 function PANEL:OnMousePressed(mcode)
     if not self:GetParent().OnMousePressed then return end
+
     return self:GetParent():OnMousePressed(mcode)
 end
 
@@ -175,6 +175,7 @@ function PANEL:AnimSlide(anim, delta, data)
     if anim.Started then
         if not IsValid(self.Contents) and (self.OldHeight or 0) < self.Header:GetTall() then
             self.OldHeight = 0
+
             for id, pnl in pairs(self:GetChildren()) do
                 self.OldHeight = self.OldHeight + pnl:GetTall()
             end
@@ -187,11 +188,12 @@ function PANEL:AnimSlide(anim, delta, data)
         end
     end
 
-    if IsValid(self.Contents) then self.Contents:SetVisible(true) end
+    if IsValid(self.Contents) then
+        self.Contents:SetVisible(true)
+    end
 
     self:GetParent():InvalidateLayout()
     self:GetParent():GetParent():InvalidateLayout()
-
     self:SetTall(Lerp(delta, data.From, data.To))
 end
 
