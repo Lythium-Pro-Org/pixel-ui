7b298de72da522cb2485314a96bbcdf7bb545603
Lythium4848 <62567544+Lythium4848@users.noreply.github.com>
2023-04-29 23:36:53 +0100

    🐛 fix: TextEntryInternal not passing value to parent AllowInput

--- lua/pixelui/elements/cl_text_entry_internal.lua
+++ lua/pixelui/elements/cl_text_entry_internal.lua
@@ -239,7 +239,7 @@ function PANEL:AllowInput(value)
     if not parent then return end
 
     if parent.AllowInput then
-        parent:AllowInput()
+        parent:AllowInput(value)
     end
 end
 
