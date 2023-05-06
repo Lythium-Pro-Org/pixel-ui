c6ee8548ded48eef7db42efaacee4d1560f06d54
Lythium4848 <62567544+Lythium4848@users.noreply.github.com>
2023-05-06 14:54:38 +0100

    ♻️ Remove print from PIXEL.Slider

--- lua/pixelui/elements/cl_slider.lua
+++ lua/pixelui/elements/cl_slider.lua
@@ -60,7 +60,6 @@ function PANEL:OnMousePressed()
     self:OnValueChanged(self.Fraction)
     self:InvalidateLayout()
     self.Grip:RequestFocus()
-    print(self.Grip:HasFocus())
 end
 
 function PANEL:OnValueChanged(fraction)
@@ -77,6 +76,9 @@ function PANEL:PerformLayout(w, h)
     local offset = PIXEL.Scale(3)
     self.Grip:SetSize(gripSize, gripSize)
     self.Grip:SetPos((self.Fraction * w) - (gripSize * .5), -offset)
+end
+
+function PANEL:LayoutContent()
 end
 
 vgui.Register("PIXEL.Slider", PANEL, "PIXEL.Button")
