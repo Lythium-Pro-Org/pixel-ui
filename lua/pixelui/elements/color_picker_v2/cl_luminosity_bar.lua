b419dae5ce97a88e1e7840ff876dc9ee1e589f71
Lythium4848 <62567544+Lythium4848@users.noreply.github.com>
2023-06-13 15:47:02 +0100

    more color picker stuff

--- lua/pixelui/elements/color_picker_v2/cl_luminosity_bar.lua
+++ lua/pixelui/elements/color_picker_v2/cl_luminosity_bar.lua
@@ -1,23 +1,39 @@
 local PANEL = {}
-local floor = math.floor
 local clamp = math.Clamp
-local colToHSV, HSVToCol = ColorToHSV, HSVToColor
 local scale = PIXEL.Scale
-AccessorFunc(PANEL, "RGBValue", "RGB")
+
+AccessorFunc(PANEL, "BaseColor", "BaseColor", FORCE_NUMBER)
+AccessorFunc(PANEL, "Hue", "Hue", FORCE_NUMBER)
+AccessorFunc(PANEL, "Saturation", "Saturation", FORCE_NUMBER)
+AccessorFunc(PANEL, "Luminosity", "Luminosity", FORCE_NUMBER)
 
 function PANEL:Init()
-    self:SetRGB(color_white)
-    self.LastX = 0
+    self:SetHue(0)
+    self:SetSaturation(100)
+    self:SetLuminosity(50)
     self.Steps = {}
 end
 
+function PANEL:SetBaseColor(color)
+    self.BaseColor = color
+    self:GenerateGradient()
+end
+
 function PANEL:PerformLayout(w, h)
+    self:GenerateGradient()
+
+    if not self.LastX then
+        self.LastX = w / 2
+    end
+end
+
+function PANEL:GenerateGradient()
     self.Steps = {}
-    self.Times = 360 -- The max number that the hue can be
+    self.Times = 100 -- The max number that the hue can be
 
     for i = 0, self.Times do
         local step = (1 / self.Times) * i
-        local color = HSVToCol(i, 1, 1)
+        local color = HSLToColor(self:GetHue(), self:GetSaturation(), i / 100)
 
         self.Steps[i] = {
             offset = step,
@@ -26,29 +42,24 @@ function PANEL:PerformLayout(w, h)
     end
 end
 
-function PANEL:GetPosColor(value)
-    local position = floor(value * self.Times)
-    local color = self.Steps[position].color
+function PANEL:GetColor()
+    local h = self:GetHue()
+    local s = self:GetSaturation()
+    local l = self:GetLuminosity()
 
-    return color
-end
-
-function PANEL:GetColorPos(color)
-    local h, _, _ = colToHSV(color)
-    local pos = floor((h * self:GetWide()) / self.Times)
-    return pos
+    return HSLToColor(h, s, l)
 end
 
 function PANEL:OnCursorMoved(x, y)
     if not input.IsMouseDown(MOUSE_LEFT) then return end
     local wide = x / self:GetWide()
-    local value = clamp(wide, 0, 1)
-    local col = self:GetPosColor(value)
+    local luminosity = clamp(wide, 0, 1)
+    self:SetLuminosity(luminosity)
+
+    local col = self:GetColor(hue)
 
     if col then
-        self.RGBValue = col
-        self.RGBValue.a = 255
-        self:OnChange(self.RGBValue)
+        self:OnChange(self:GetLuminosity())
     end
 
     self.LastX = x
@@ -77,6 +88,8 @@ function PANEL:Paint(w, h)
         PIXEL.DrawLinearGradient(x, y, w, h, self.Steps, true)
     end)
 
+    if not self.LastX then return end
+
     local newX = self.LastX
 
     if newX < (h / 2) then
@@ -89,7 +102,7 @@ function PANEL:Paint(w, h)
 
     PIXEL.DrawFullRoundedBox(8, newX - (h / 2), 0, h, h, color_white)
     x, y, wh = newX + scale(3), scale(3), h - scale(6)
-    PIXEL.DrawFullRoundedBox(4, x - (h / 2), y, wh, wh, self:GetRGB())
+    PIXEL.DrawFullRoundedBox(4, x - (h / 2), y, wh, wh, self:GetColor())
 end
 
-vgui.Register("PIXEL.RGBPicker", PANEL, "EditablePanel")
+vgui.Register("PIXEL.LuminosityBar", PANEL, "EditablePanel")
